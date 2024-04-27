5830a6e6742f010a20bd1563f392f4deb41be9dd
HowardYuuu <asportboy235@gmail.com>
2024-04-27 15:16:55 +0800

    4/27 更新

--- 精誠微雲_古皓宇sln/Controllers/HomeController.cs
+++ 精誠微雲_古皓宇sln/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
 using 精誠微雲_古皓宇sln.Models;
 using 精誠微雲_古皓宇sln.ViewModels;
 
@@ -17,8 +19,49 @@ namespace 精誠微雲_古皓宇sln.Controllers
 			_dbBankContext = dbBankContext;
 		}
 
+
 		public IActionResult Index()
 		{
+			List<string> stringList = new List<string>();
+			stringList.Add("ABC");
+			stringList.Add("123");
+			stringList.Add("456");
+
+			@ViewBag.Ans_1 = stringList.Where(x => x == "123").FirstOrDefault();
+
+
+			List<Person> people = new List<Person>();
+
+			Person person1 = new Person();
+			person1.Name = "張三";
+			person1.Gender = "男";
+			person1.BirthDate = new DateTime(1998, 07, 14);
+			people.Add(person1);
+
+			Person person2 = new Person();
+			person2.Name = "李四";
+			person2.Gender = "男";
+			person2.BirthDate = new DateTime(1996, 06, 13);
+			people.Add(person2);
+
+			Person person3 = new Person();
+			person3.Name = "倩倩";
+			person3.Gender = "女";
+			person3.BirthDate = new DateTime(1996, 06, 13);
+			people.Add(person3);
+
+			List<Person> personList = new List<Person>();
+			foreach (var person in people)
+			{
+				personList.Add(person);
+			}
+			@ViewBag.Ans_2 = personList
+				.Where(x => x.Gender == "女" && x.BirthDate.Value == new DateTime(1996, 06, 13).Date)
+				.Select(x => x.Name).FirstOrDefault();
+
+			ViewBag.Ans_3 = GenerateRandomNumber(50);
+
+
 			var result = from n in _dbBankContext.NetBankUsers
 						 join a in _dbBankContext.Accounts
 						 on n.UserGuid equals a.OwnerGuid
@@ -29,12 +72,35 @@ namespace 精誠微雲_古皓宇sln.Controllers
 							 BranchId = a.BranchId,
 							 AcctSerialId = a.AcctSerialId
 						 };
-			if(result == null)
+			if (result == null)
 			{
 				return NotFound();
 			}
 
 			return View(result);
+		}
+
+		static string GenerateRandomNumber(int length)
+		{
+			if (length <= 0)
+			{
+				string message = "長度需大於0";
+				return message;
+			}
+			using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+			{
+				byte[] randomNumber = new byte[length];
+				rng.GetBytes(randomNumber);
+			
+				StringBuilder result = new StringBuilder(length);
+				foreach (byte b in randomNumber)
+				{
+					int number = b % 10;
+					result.Append(number);
+				}
+
+				return result.ToString();
+			}
 		}
 
 		public IActionResult Privacy()
---
+++ 精誠微雲_古皓宇sln/ViewModels/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace 精誠微雲_古皓宇sln.ViewModels
+{
+	public class Person
+	{
+		public string? Name { get; set; }
+		public string? Gender { get; set; }
+		public DateTime? BirthDate { get; set; }
+	}
+}
